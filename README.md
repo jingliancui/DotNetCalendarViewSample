3151eedf1d2f02881b530f148d2335250a85c060
jingliancui <ericcui@qq.com>
2021-04-28 21:52:26 +0800

    Add sample code.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # XamarinFormsCalendarViewSample
 Sample for showing CalendarView on xamarin.forms
+
+Tutorial:
+
+https://mp.weixin.qq.com/s/sunKdjrJDb4E_S4mpHShTA
+
+QQ Group:313308215
+
+WeChat Official Account(微信公众号):
+
+Xamarin Library
+
+<img src="https://github.com/jingliancui/XamarinFormsMVVMSample/blob/master/Images/wechatqrcode.jpg?raw=true"/>
---
+++ SampleApp/SampleApp.Android/Renderers/XCalendarViewRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using Android.Content;
+using Android.Views;
+using Com.Haibin.Calendarview;
+using SampleApp.Controls;
+using SampleApp.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(XCalendarView), typeof(XCalendarViewRenderer))]
+namespace SampleApp.Droid.Renderers
+{
+    public class XCalendarViewRenderer :ViewRenderer<XCalendarView, CalendarView >
+    {
+        private CalendarView calendarView;
+
+        private Context context;
+
+        public XCalendarViewRenderer(Context _context):base(_context)
+        {
+            context = _context;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<XCalendarView> e)
+        {
+            calendarView = new CalendarView(context);
+            SetNativeControl(calendarView);
+        }
+    }
+}
